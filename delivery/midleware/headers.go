9e383df4819488422a09ea61687f8de0f3819811
Savko Aleksandr <a.savko@ecos.am>
2024-10-12 12:20:39 +0300

    Fixed middleware respnse after logging

---
+++ delivery/midleware/headers.go
@@ -0,0 +1,8 @@
+package midleware
+
+//func AddHeadersMiddleware(e echo.Context) error {
+//	response := e.Response()
+//	response.Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+//	body := response
+//	response.Header().Set(echo.HeaderContentLength, echo.MIMEApplicationJSON)
+//}
