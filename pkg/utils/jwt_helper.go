b05367fbe012fd5625d07d4f41366afbaedfd1dc
Savko Aleksandr <a.savko@ecos.am>
2024-10-13 02:33:53 +0300

    Refactored validate token function

--- pkg/utils/jwt_helper.go
+++ pkg/utils/jwt_helper.go
@@ -70,6 +70,7 @@ func GenerateTokens(userID uint64) (*TokenDetails, error) {
 // ValidateToken проверяет корректность и валидность токена (Access или Refresh)
 func ValidateToken(tokenString string) (*jwt.Token, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Проверка метода подписи токена (HMAC)
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
@@ -81,8 +82,37 @@ func ValidateToken(tokenString string) (*jwt.Token, error) {
 	}
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		logrus.Infof("User ID from token: %v", claims["user_id"])
-		return token, nil // Возвращаем валидный токен
+		// Проверяем наличие user_id в токене
+		userID, ok := claims["user_id"].(float64)
+		if !ok {
+			return nil, fmt.Errorf("invalid token: user_id missing or invalid")
+		}
+		//TODO: Добавить добавления токена в БД типо redis для управления сессиями
+		// Проверяем наличие UUID токена
+		tokenUUID, ok := claims["access_uuid"].(string)
+		if !ok {
+			return nil, fmt.Errorf("invalid token: UUID missing or invalid")
+		}
+
+		// Проверяем время истечения токена
+		exp, ok := claims["exp"].(float64)
+		if !ok {
+			return nil, fmt.Errorf("invalid token: expiration time missing")
+		}
+
+		// Текущее время в Unix формате
+		currentTime := time.Now().Unix()
+
+		// Если время истекло, возвращаем ошибку
+		if int64(exp) < currentTime {
+			return nil, fmt.Errorf("token has expired")
+		}
+
+		// Логируем полезные данные из токена
+		logrus.Infof("User ID from token: %v, Token UUID: %v", userID, tokenUUID)
+
+		// Если все проверки пройдены, возвращаем валидный токен
+		return token, nil
 	}
 
 	return nil, fmt.Errorf("invalid token")
