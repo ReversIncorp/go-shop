261a740a5006f46e82c93e0cbd4f2fc5bb510f8f
delaver dev <delaverdev@gmail.com>
2024-10-30 00:13:45 +0300

    refactored postgres connect

--- pkg/database/postgreSQL.go
+++ pkg/database/postgreSQL.go
@@ -10,9 +10,19 @@ import (
 )
 
 func OpenPostgreSQL() (*sql.DB, error) {
-	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"))
+	connStr := fmt.Sprintf(""+
+		"host=%s "+
+		"port=%s "+
+		"user=%s "+
+		"password=%s "+
+		"dbname=%s "+
+		"sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
