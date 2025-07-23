0572e2d50e6ede845d241b25479b3823035e770b
Maut <vadimbudrik@gmail.com>
2025-07-23 21:19:57 +0300

    feat(docker): using .env in .sh scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ go mod download
 go install github.com/pressly/goose/v3/cmd/goose@latest
 
 echo "Running database migrations..."
-goose -dir pkg/database/migrations postgres "host=db user=marketplace password=12345 dbname=go-shop sslmode=disable" up
+goose -dir pkg/database/migrations postgres "host=${DB_HOST} user=${DB_USER} password=${DB_PASSWORD} dbname=${DB_NAME} sslmode=disable" up
 
 echo "Starting application..."
-go run app/main.go 
+go run app/main.go
