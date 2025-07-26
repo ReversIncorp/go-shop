2d54e9266d85c3d7b74bb48a8345e79a72b3df22
Maut <vadimbudrik@gmail.com>
2025-07-26 15:50:38 +0300

    fix(docker): changed Dockerfile and deleted ProdDockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 set -e
 
-go mod download
-go install github.com/pressly/goose/v3/cmd/goose@latest
+# Wait for PostgreSQL
+until pg_isready -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}"; do
+  echo "Waiting for PostgreSQL..."
+  sleep 1
+done
 
 echo "Running database migrations..."
-goose -dir pkg/database/migrations postgres "host=${DB_HOST} user=${DB_USER} password=${DB_PASSWORD} dbname=${DB_NAME} sslmode=disable" up
+goose -dir pkg/database/migrations postgres "host=${DB_HOST} port=${DB_PORT} user=${DB_USER} password=${DB_PASSWORD} dbname=${DB_NAME} sslmode=disable" up
 
 echo "Starting application..."
-go run app/main.go
+./main
